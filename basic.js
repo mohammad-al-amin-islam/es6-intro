fa166cc7afeca3a953d84e4a9d656f0a66e92243
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-19 09:43:48 +0600

    basic and dom revised practice added

---
+++ basic.js
@@ -0,0 +1,17 @@
+var number = 23;
+//6 comparison operator
+// >,<,==,!=,<=,>=;
+
+// comparison if else
+
+//array
+const array = [1,3,4,5];
+array.push(5);
+array.pop();
+//to change position array[2] = 555;
+
+// array.indexOf(222) !=  -1
+
+//array.includes(34);
+//while ,for loop,for of loop
+// function object
---
+++ dom.js
@@ -0,0 +1,25 @@
+document.getElementById('add-border').addEventListener('click',function(){
+    document.getElementById('friends').style.border = "5px solid red";
+});
+
+function changeBackgroundColor(){
+    const friends = document.getElementsByClassName('friend');
+    for(const friend of friends){
+        friend.style.backgroundColor = "lightblue";
+    }
+};
+
+document.getElementById('add-friend').addEventListener('click',function(){
+    const getSection = document.getElementById('friends');
+    const addDiv = document.createElement('div');
+    addDiv.classList.add('friend');
+    addDiv.innerHTML = `
+            <h3 class="friends-name">New Friend</h3>
+            <p>Reprehenderit, repellendus.</p>
+    `
+    getSection.appendChild(addDiv);
+});
+
+function addPadding(){
+    document.getElementById('friends').style.padding ="10px"
+}
